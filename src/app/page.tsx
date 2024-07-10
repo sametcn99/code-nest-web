71b3f94a1be5b513eb16a1454c92c4f0c8f8955d
samet <sametcn99@gmail.com>
2024-07-10 23:59:40 +0300

    chore: Remove unused clerk component and update Supabase client usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Auth from "@/components/Auth";
 import Editor from "@/components/editor/Editor";
 
 export default function Home() {
   return (
     <main>
       <Editor />
-      <Auth />
     </main>
   );
 }
