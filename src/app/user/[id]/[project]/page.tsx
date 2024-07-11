2f1113aa25f6a70898aa58ff64cfa40af978b139
samet <sametcn99@gmail.com>
2024-07-11 03:57:55 +0300

    Refactor page component to include 'id' in params

--- src/app/user/[id]/[project]/page.tsx
+++ src/app/user/[id]/[project]/page.tsx
@@ -1,4 +1,7 @@
-export default function Page({ params }: { params: { project: string } }) {
-    return <div>{params.project}</div>;
-  }
-  
+export default function Page({
+  params,
+}: {
+  params: { project: string; id: string };
+}) {
+  return <div>{params.project}</div>;
+}
