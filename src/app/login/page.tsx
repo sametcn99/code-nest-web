2c67c164f2d5130ec092e6f8ecbd32ccbb244a73
samet <sametcn99@gmail.com>
2024-07-11 04:54:45 +0300

    chore: Update Supabase client usage and remove unused clerk component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,13 @@
+import { createClient } from "@/lib/utils/supabase/server";
 import { signInWithDiscord } from "./actions";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const supabase = createClient();
+  const { data } = await supabase.auth.getUser();
+  if (data?.user) {
+    redirect("/me");
+  }
   return (
     <form>
       <button formAction={signInWithDiscord}>Sign In With Discord</button>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,36 @@
-"use client";
 import Link from "next/link";
-import React from "react";
+import { createClient } from "@/lib/utils/supabase/server";
+import SignOut from "./SignOut";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const supabase = createClient();
+  const { data } = await supabase.auth.getUser();
   return (
-    <div className="flex flex-row justify-between">
-      <div className="inline-flex gap-2">
-        <Link href={"/"} target="_blank">
-          Home
-        </Link>{" "}
-        <Link href={"/explore"} target="_blank">
-          Explore
-        </Link>
+    <>
+      <div className="flex flex-row justify-between">
+        <div className="inline-flex gap-2">
+          <Link href={"/"} target="_blank">
+            Home
+          </Link>
+          <Link href={"/explore"} target="_blank">
+            Explore
+          </Link>
+        </div>
+        <div className="inline-flex gap-2">
+          {data?.user ? (
+            <>
+              <Link href={"/me"} target="_blank">
+                Profile
+              </Link>
+              <SignOut />
+            </>
+          ) : (
+            <Link href={"/login"} target="_blank">
+              Login
+            </Link>
+          )}
+        </div>
       </div>
-      <div className="inline-flex">
-        <Link href={"/me"}>Profile</Link>
-      </div>
-    </div>
+    </>
   );
 }
