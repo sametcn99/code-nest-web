8856fb44b601b997ef36cf28560a8b96008d9681
samet <sametcn99@gmail.com>
2024-07-12 19:57:15 +0300

    sc

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,32 +2,15 @@ import { createClient } from "@/lib/utils/supabase/server";
 import Link from "next/link";
 import UserButton from "./UserButton";
 import { FloatingNavbar } from "./ui/FloatingNavbar";
-import MiniNavbar from "./ui/MiniNavbar";
 
 export default async function Navbar() {
   const supabase = createClient();
   const { data } = await supabase.auth.getUser();
   return (
-    <>
-      {/* <div className="flex w-full flex-row justify-between p-2">
-        <div className="inline-flex gap-2">
-          <Link href={"/"} className="nav-item">
-            Ana Sayfa
-          </Link>
-          <Link href={"/explore"} className="nav-item">
-            Keşfet
-          </Link>
-        </div>
-        <div className="inline-flex gap-2">
-          <UserButton
-            avatar_url={data?.user?.user_metadata.avatar_url}
-            username={data.user?.user_metadata.full_name}
-          />
-        </div>
-      </div>
-     */}
-      <MiniNavbar navItems={navItems} username={data.user?.user_metadata.full_name} />
-    </>
+    <FloatingNavbar
+      navItems={navItems}
+      username={data.user?.user_metadata.full_name}
+    />
   );
 }
 
--- src/components/ui/FloatingNavbar.tsx
+++ src/components/ui/FloatingNavbar.tsx
@@ -22,24 +22,7 @@ export const FloatingNavbar = ({
   username: string | null;
   className?: string;
 }) => {
-  const { scrollYProgress } = useScroll();
-
-  const [visible, setVisible] = useState(false);
-
-  useMotionValueEvent(scrollYProgress, "change", (current) => {
-    // Check if current is not undefined and is a number
-    if (typeof current === "number") {
-      let direction = current! - scrollYProgress.getPrevious()!;
-
-      if (scrollYProgress.get() < 0.2) {
-        setVisible(false);
-      } else {
-        if (direction > 0) {
-          setVisible(true);
-        }
-      }
-    }
-  });
+  const [visible, setVisible] = useState(true);
 
   return (
     <AnimatePresence mode="wait">
@@ -56,7 +39,7 @@ export const FloatingNavbar = ({
           duration: 0.2,
         }}
         className={cn(
-          "fixed inset-x-0 top-5 z-[5000] mx-auto flex max-w-fit flex-wrap items-center justify-center space-x-4 rounded-full border border-transparent bg-white py-2 pl-8 pr-2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-white/[0.2] dark:bg-black",
+          "sticky inset-x-0 top-5 z-[5000] mx-auto flex max-w-fit flex-wrap items-center justify-center space-x-4 rounded-full border border-transparent backdrop-blur-xl py-2 pl-8 pr-2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-white/[0.2] ",
           className,
         )}
       >
@@ -68,8 +51,8 @@ export const FloatingNavbar = ({
               "relative flex items-center space-x-1 text-neutral-600 hover:text-neutral-500 dark:text-neutral-50 dark:hover:text-neutral-300",
             )}
           >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden text-sm sm:block">{navItem.name}</span>
+            <span>{navItem.icon}</span>
+            <span>{navItem.name}</span>
           </Link>
         ))}
         <Link
