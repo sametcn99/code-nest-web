617f6553e836000d401d51d21b72189ac569d4f4
samet <sametcn99@gmail.com>
2024-07-12 06:38:19 +0300

    sc

---
+++ src/components/SignOut.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createClient } from '@/lib/utils/supabase/client';
+
+export default function SignOut() {
+  const supabase = createClient();
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    window.location.href = '/login';
+  };
+
+  return (
+    <button type="button" onClick={handleSignOut} className="text-xl">
+      Çıkış Yap
+    </button>
+  );
+}
