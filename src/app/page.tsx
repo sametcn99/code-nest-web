5529e6f51eedc739a38ffaa7c91984bc54facf8d
samet <sametcn99@gmail.com>
2024-07-12 06:49:40 +0300

    sc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import { createClient } from "@/lib/utils/supabase/server";
 import Link from "next/link";
+import { FaDiscord } from "react-icons/fa";
 
 export default async function Home() {
+  const supabase = createClient();
+  const {data , error} = await supabase.auth.getUser();
+  const user = data;
+
   return (
-    <main className="mx-auto flex h-[60%] w-fit flex-col place-items-center justify-center">
+    <main className="mx-auto flex h-[60%] w-fit flex-col place-items-center justify-center gap-2">
       <div>
         <h1 className="flex w-full items-center justify-center text-6xl font-semibold">
           CODENEST
@@ -14,17 +20,28 @@ export default async function Home() {
       <div className="flex h-auto items-center justify-center gap-[0.9375rem] px-[1.5625rem]">
         <Link
           href={"/new"}
-          className="font-montserrat flex h-10 cursor-pointer items-center justify-center rounded-2xl border border-gray-500 px-6 text-gray-200 transition-all duration-200 ease-in-out hover:text-gray-500"
+          className="flex h-10 cursor-pointer items-center justify-center rounded-2xl border border-gray-500 px-6 text-gray-200 transition-all duration-200 ease-in-out hover:text-gray-500"
         >
           Başlayın
         </Link>
-        <Link
-          href={"/login"}
-          className="font-montserrat flex h-10 cursor-pointer items-center justify-center rounded-2xl border border-gray-500 px-6 text-gray-200 transition-all duration-200 ease-in-out hover:text-gray-500"
-        >
-          Giriş Yap
-        </Link>
+        {!user.user ? (
+          <Link
+            href={"/login"}
+            className="flex h-10 cursor-pointer items-center justify-center rounded-2xl border border-gray-500 px-6 text-gray-200 transition-all duration-200 ease-in-out hover:text-gray-500"
+          >
+            Giriş Yap
+          </Link>
+        ) : (
+          <></>
+        )}
       </div>
+      <Link
+        href={"/"}
+        className="flex h-10 cursor-pointer items-center justify-center gap-2 rounded-2xl border border-gray-500 px-6 text-gray-200 transition-all duration-200 ease-in-out hover:text-gray-500"
+      >
+        <FaDiscord size={22} />
+        <span>Topluluğa katılın</span>
+      </Link>
     </main>
   );
 }
