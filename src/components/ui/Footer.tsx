a5cbd7fd2801c657059e423663dd59e6f43be7ae
samet <sametcn99@gmail.com>
2024-07-15 16:48:06 +0300

    fix footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,51 +1,48 @@
 import Link from "next/link";
-import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaTwitter, FaGithub, FaLinkedin, FaDiscord } from "react-icons/fa";
+import { TbWorld } from "react-icons/tb";
 
 export default function Footer() {
   return (
-    <footer className="mx-auto w-full py-8 text-gray-300 sm:w-[80%] md:w-[60%]">
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col items-center justify-between gap-[20px] md:flex-row">
-          <div className="mb-4 text-center md:mb-0 md:text-left">
-            <h2 className="text-xl font-semibold text-white">CODENEST</h2>
-          </div>
-          <div className="mb-4 flex flex-wrap justify-center space-x-4 md:mb-0">
-            {items.map((item, index) => (
-              <Link className="hover:text-white" href={item.link} key={index}>
-                {item.title}
-              </Link>
-            ))}
-          </div>
-          <div className="mt-4 flex justify-center space-x-4 md:mt-0">
-            <Link
-              href="https://twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-white"
-            >
-              <FaTwitter size={24} />
-            </Link>
-            <Link
-              href="https://sametcc.me/code-nest-web"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-white"
-            >
-              <FaGithub size={24} />
-            </Link>
-            <Link
-              href="https://linkedin.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-white"
-            >
-              <FaLinkedin size={24} />
-            </Link>
-          </div>
-        </div>
-        <div className="mt-8 border-t border-gray-700 pt-4 text-center text-gray-500">
-          <p>&copy; 2024 Codenst. Tüm hakları saklıdır.</p>
-        </div>
+    <footer className="container mx-auto flex min-h-[30rem] flex-col justify-end px-4 text-gray-300">
+      <div className="mb-4 text-center md:mb-0 md:text-left">
+        <h2 className="text-xl font-semibold text-white">CODENEST</h2>
+      </div>
+      <div className="mb-4 flex flex-wrap justify-center space-x-4">
+        {items.map((item, index) => (
+          <Link className="hover:text-white" href={item.link} key={index}>
+            {item.title}
+          </Link>
+        ))}
+      </div>
+      <div className="mt-4 flex justify-center space-x-4 md:mt-0">
+        <Link
+          href="https://sametcc.me/code-nest-web"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-white"
+        >
+          <FaGithub size={24} />
+        </Link>
+        <Link
+          href="https://discord.gg/PtP372mA"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-white"
+        >
+          <FaDiscord size={24} />
+        </Link>
+        <Link
+          href="https://code-nest-web.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-white"
+        >
+          <TbWorld size={24} />
+        </Link>
+      </div>
+      <div className="mt-8 border-t border-gray-700 pt-4 text-center text-gray-500">
+        <p>&copy; 2024 CodeNest. Tüm hakları saklıdır.</p>
       </div>
     </footer>
   );
