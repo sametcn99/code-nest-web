b00ea2208cfdb0a24e1736f955f67e6a300615e6
samet <sametcn99@gmail.com>
2024-07-20 07:55:11 +0300

    sc

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { type NextRequest } from "next/server";
 import { visitMiddleware } from "./middlewares/visit_middleware";
-import { updateSession } from "./middlewares/supabase_middleware";
 
 /**
  * Executes the middleware function for the given request.
@@ -8,7 +7,7 @@ import { updateSession } from "./middlewares/supabase_middleware";
  * @returns A Promise that resolves to the result of the updateSession function.
  */
 export async function middleware(request: NextRequest) {
-  return await visitMiddleware(request), updateSession(request);
+  return await visitMiddleware(request);
 }
 
 /**
--- src/middlewares/visit_middleware.ts
+++ src/middlewares/visit_middleware.ts
@@ -30,57 +30,57 @@ export async function visitMiddleware(request: NextRequest) {
 
     const supabase = createClient();
     const { data: user } = await supabase.auth.getUser();
-    if (user.user !== null) return visitResponse;
+    if (user.user === null) {
+      const cookieStore = cookies().getAll();
 
-    const cookieStore = cookies().getAll();
+      const visitDataCookie = request.cookies.get("visit_data")?.value;
+      let visitData: VisitData = visitDataCookie
+        ? JSON.parse(visitDataCookie)
+        : null;
 
-    const visitDataCookie = request.cookies.get("visit_data")?.value;
-    let visitData: VisitData = visitDataCookie
-      ? JSON.parse(visitDataCookie)
-      : null;
+      const now = new Date();
+      const resetVisitData = (ip: string): VisitData => ({
+        ip,
+        visit_count: 1,
+        first_visit: now,
+        last_visit: now,
+      });
 
-    const now = new Date();
-    const resetVisitData = (ip: string): VisitData => ({
-      ip,
-      visit_count: 1,
-      first_visit: now,
-      last_visit: now,
-    });
+      if (visitData) {
+        const firstVisitDate = new Date(visitData.first_visit);
 
-    if (visitData) {
-      const firstVisitDate = new Date(visitData.first_visit);
+        const isSameDay = (date1: Date, date2: Date): boolean =>
+          date1.getDate() === date2.getDate() &&
+          date1.getMonth() === date2.getMonth() &&
+          date1.getFullYear() === date2.getFullYear();
 
-      const isSameDay = (date1: Date, date2: Date): boolean =>
-        date1.getDate() === date2.getDate() &&
-        date1.getMonth() === date2.getMonth() &&
-        date1.getFullYear() === date2.getFullYear();
+        if (visitData.ip === ipAddress && isSameDay(firstVisitDate, now)) {
+          visitData.visit_count += 1;
+          visitData.last_visit = now;
 
-      if (visitData.ip === ipAddress && isSameDay(firstVisitDate, now)) {
-        visitData.visit_count += 1;
-        visitData.last_visit = now;
-
-        if (visitData.visit_count > MAX_VISITS_PER_DAY) {
-          const url = request.nextUrl.clone();
-          if (url.searchParams.get("error") === "rate_limit_exceeded")
-            return visitResponse;
-          url.searchParams.set("error", "rate_limit_exceeded");
-          return NextResponse.redirect(url);
+          if (visitData.visit_count > MAX_VISITS_PER_DAY) {
+            const url = request.nextUrl.clone();
+            if (url.searchParams.get("error") === "rate_limit_exceeded")
+              return visitResponse;
+            url.searchParams.set("error", "rate_limit_exceeded");
+            return NextResponse.redirect(url);
+          }
+        } else {
+          visitData = resetVisitData(ipAddress);
         }
       } else {
         visitData = resetVisitData(ipAddress);
       }
-    } else {
-      visitData = resetVisitData(ipAddress);
-    }
 
-    cookieStore.push({
-      name: "visit_data",
-      value: JSON.stringify(visitData),
-    });
-    cookieStore.forEach((cookie) => {
-      visitResponse.cookies.set(cookie);
-    });
-    return visitResponse;
+      cookieStore.push({
+        name: "visit_data",
+        value: JSON.stringify(visitData),
+      });
+      cookieStore.forEach((cookie) => {
+        visitResponse.cookies.set(cookie);
+      });
+      return visitResponse;
+    }
   } catch (error) {
     console.error("Error in visitMiddleware:", error);
     return NextResponse.next();
