e59846377c6648856212cc2c76ba71b306d9caf9
samet <sametcn99@gmail.com>
2024-07-20 17:46:44 +0300

    sc

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,11 @@ import { updateSession } from "./middlewares/supabase_middleware";
  * @returns A Promise that resolves to the result of the updateSession function.
  */
 export async function middleware(request: NextRequest) {
-  return;
+  const composeMiddlewares = async (request: NextRequest) => {
+    await updateSession(request);
+    await visitMiddleware(request);
+  };
+  return await composeMiddlewares(request);
 }
 
 /**
