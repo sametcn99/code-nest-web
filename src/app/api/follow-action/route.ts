5a18aca2d6353189a22fea9de864b1ec28947d9d
samet <sametcn99@gmail.com>
2024-07-22 05:21:26 +0300

    fix follow

--- src/app/api/follow-action/route.ts
+++ src/app/api/follow-action/route.ts
@@ -10,42 +10,56 @@ export async function POST(req: NextRequest) {
   try {
     const { user, viewerId, action } = await req.json();
     const supabase = createClient();
-    let followingList: string[] = user.followings ?? [];
+
+    const { data: viewerData } = await supabase
+      .from("profiles")
+      .select("followings")
+      .eq("id", viewerId)
+      .single();
+
+    let followingList: string[] = [];
     let followersList: string[] = user.followers ?? [];
     if (action === "Follow") {
+      if (viewerData) {
+        followingList = viewerData.followings ?? [];
+      }
       followingList.push(user.id);
+
+      const { error } = await supabase
+        .from("profiles")
+        .update({
+          followings: followingList,
+        })
+        .eq("id", viewerId);
+
       followersList.push(viewerId);
-      const { error } = await supabase
-        .from("profiles")
-        .update({
-          followings: followingList,
-        })
-        .eq("id", viewerId);
-
       const { error: error2 } = await supabase
         .from("profiles")
         .update({
           followers: followersList,
         })
         .eq("id", user.id);
-
       if (error2) throw error2;
       if (error) throw error;
-      else {
-        return NextResponse.json({
-          response: "success",
-          status: 200,
-        });
-      }
+
+      return NextResponse.json({
+        response: "success",
+        status: 200,
+      });
     } else if (action === "Unfollow") {
-      followingList.splice(followingList.indexOf(user.id), 1);
-      followersList.splice(followersList.indexOf(viewerId), 1);
+      if (viewerData) {
+        followingList = viewerData.followings ?? [];
+        followingList.splice(followingList.indexOf(user.id), 1);
+      }
+
       const { error } = await supabase
         .from("profiles")
         .update({
           followings: followingList,
         })
         .eq("id", viewerId);
+
+      followersList.splice(followersList.indexOf(viewerId), 1);
       const { error: error2 } = await supabase
         .from("profiles")
         .update({
@@ -55,12 +69,10 @@ export async function POST(req: NextRequest) {
 
       if (error) throw error;
       if (error2) throw error2;
-      else {
-        return NextResponse.json({
-          response: "success",
-          status: 200,
-        });
-      }
+      return NextResponse.json({
+        response: "success",
+        status: 200,
+      });
     }
   } catch (error) {
     return NextResponse.json({
