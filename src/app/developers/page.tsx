2ba2c765d2e8e6311c04fa9e3e90d1b3ccecffc6
samet <sametcn99@gmail.com>
2024-07-24 03:52:34 +0300

    fix

--- src/app/developers/page.tsx
+++ src/app/developers/page.tsx
@@ -1,5 +1,7 @@
-import ProfileCard from "@/components/ProfileCard";
 import { createClient } from "@/utils/server";
+import { Card, CardHeader } from "@nextui-org/react";
+import Image from "next/image";
+import Link from "next/link";
 import { FaCrown } from "react-icons/fa6";
 import { Tables } from "../../../types/supabase";
 
@@ -22,7 +24,28 @@ export default async function Page() {
       <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2">
         {users && users.length > 0 ? (
           users.map((user, index) => (
-            <ProfileCard user={user} auth={false} key={index} className="" />
+            <Link href={`/user/${user.id}`} key={index}>
+              <Card key={index} className="p-2 hover:scale-101">
+                <CardHeader className="inline-flex gap-3">
+                  <Image
+                    src={user.avatar_url || "/images/default_avatar.png"}
+                    alt={`${user.username || user.full_name}'s Avatar`}
+                    width={100}
+                    height={100}
+                    draggable={false}
+                    title={`${user.username || user.full_name}'s Avatar`}
+                    className="rounded-xl"
+                  />
+                  <div className="flex flex-col text-sm">
+                    <h2 className="text-2xl font-bold">
+                      {user.username || user.full_name}
+                    </h2>
+                    <p>{user.roles?.join(", ")}</p>
+                    <p>{user.bio || "Bio eklenmemiş."}</p>
+                  </div>
+                </CardHeader>
+              </Card>
+            </Link>
           ))
         ) : (
           <FaCrown />
