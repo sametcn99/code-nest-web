4f64505bfa6e3fe7d3b8436089b2d0756613af78
Eren <114621741+k-eren-k@users.noreply.github.com>
2024-07-25 00:39:14 +0300

    Update route.ts

--- src/app/signout/route.ts
+++ src/app/signout/route.ts
@@ -1,20 +1,55 @@
 import { createClient } from "@/utils/server";
 import { NextResponse } from "next/server";
 
+const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL; 
+
 export async function GET(request: Request) {
   try {
     const { origin } = new URL(request.url);
     const supabase = createClient();
-    const { error } = await supabase.auth.signOut();
+    
+    // Fetch the current user
+    const authUser = await supabase.auth.getUser();
+    const { data: user, error: userError } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", authUser.data.user?.id)
+      .single();
 
-    if (error) {
+    if (userError || !user) {
+      throw Error("User not found");
+    }
+
+    // Sign out the user
+    const { error: signOutError } = await supabase.auth.signOut();
+
+    if (signOutError) {
       throw Error("Sign out failed");
     }
+
+    // Prepare the payload for Discord webhook
+    const payload = {
+      embeds: [
+        {
+          description: `${user.username} **adlı kullanıcı siteden çıkış yaptı** <a:kGif:1263073433533677568>`,
+          color: 0x2B2D31, // Dark color
+          timestamp: new Date().toISOString(), // Add timestamp if needed
+        },
+      ],
+    };
+
+    // Send the log to Discord webhook
+    await fetch(DISCORD_WEBHOOK_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
 
     return NextResponse.redirect(`${origin}/`);
   } catch (error) {
     console.error("Error during sign out:", error);
-    // Optionally, redirect to an error page or return a custom error response
     return new Response("Error processing your request", { status: 500 });
   }
 }
