56e39873f076742850f8413a43c57d4e8d2c9348
samet <sametcn99@gmail.com>
2024-07-26 00:38:46 +0300

    remove community button from navbar

--- src/components/ui/Navbar/FloatingNavbar.tsx
+++ src/components/ui/Navbar/FloatingNavbar.tsx
@@ -47,14 +47,14 @@ export const FloatingNavbar = ({ id, className }: FloatingNavbarProps) => {
           duration: 0.2,
         }}
         className={cn(
-          "sticky inset-x-0 top-5 z-[5000] mx-auto flex max-w-fit select-none flex-wrap items-center justify-center space-x-4 rounded-full border border-transparent py-2 pl-8 pr-2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] backdrop-blur-xl dark:border-white/[0.2]",
+          "sticky inset-x-0 top-5 z-[5000] mx-auto flex max-w-fit select-none flex-wrap items-center justify-center space-x-4 rounded-full border border-white/[0.2] py-2 pl-8 pr-2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] backdrop-blur-xl",
           className,
         )}
       >
         <div className="flex gap-5">
           <Link
             href="/"
-            className="bg-transparant relative flex items-center text-neutral-600 hover:text-neutral-500 dark:text-neutral-50 dark:hover:text-neutral-300"
+            className="bg-transparant relative flex place-items-center justify-center text-neutral-50 hover:text-neutral-300"
           >
             Anasayfa
           </Link>
@@ -71,19 +71,13 @@ export const FloatingNavbar = ({ id, className }: FloatingNavbarProps) => {
               </DropdownItem>
             </DropdownMenu>
           </Dropdown>
-          <Link
-            href="https://discord.gg/squMTuffqS"
-            className="bg-transparant relative flex items-center text-neutral-600 hover:text-neutral-500 dark:text-neutral-50 dark:hover:text-neutral-300"
-          >
-            Topluluk
-          </Link>
         </div>
         {id ? (
           <Dropdown>
             <DropdownTrigger>
               <Button
                 variant={"light"}
-                className="relative rounded-full border border-neutral-200 px-4 py-2 text-sm font-medium text-black dark:border-white/[0.2] dark:text-white"
+                className="relative rounded-full border border-white/[0.2] px-4 py-2 text-sm font-medium text-white"
               >
                 Profil
                 <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
@@ -109,7 +103,7 @@ export const FloatingNavbar = ({ id, className }: FloatingNavbarProps) => {
         ) : (
           <Link
             href="/login"
-            className="relative rounded-full border border-neutral-200 px-4 py-2 text-sm font-medium text-black dark:border-white/[0.2] dark:text-white"
+            className="relative rounded-full border border-white/[0.2] px-4 py-2 text-sm font-medium text-white"
           >
             <span>Giriş Yap</span>
             <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
